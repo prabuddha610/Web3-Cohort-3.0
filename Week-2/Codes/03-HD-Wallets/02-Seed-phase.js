667bfa23317793f15d98e02703aaeaf38152704b
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-12-31 01:56:43 +0530

    Updated the whole repo!

--- Week-2/Codes/03-HD-Wallets/02-Seed-phase.js
+++
@@ -1,5 +0,0 @@
-import { generateMnemonic, mnemonicToSeedSync } from "bip39";
-
-const mnemonic = generateMnemonic();
-console.log("Generated Mnemonic:", mnemonic);
-const seed = mnemonicToSeedSync(mnemonic);
